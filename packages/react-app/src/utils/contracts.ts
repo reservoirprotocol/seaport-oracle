790566ff0a9f4e34a7ea9a94f7b1b64529f38af4
George Roman <george.roman.99@gmail.com>
2023-03-14 08:18:56 +0100

    fix: cancellation zone address

--- packages/react-app/src/utils/contracts.ts
+++ packages/react-app/src/utils/contracts.ts
@@ -4,7 +4,7 @@ export type Contracts = {
 
 export const CONTRACT_ADDRESSES: Record<number, Contracts> = {
   [1]: {
-    cancellationZone: "0xe1066481cc3b038badd0c68dfa5c8f163c3ff192",
+    cancellationZone: "0xaa0e012d35cf7d6ecb6c2bf861e71248501d3226",
   },
   [5]: {
     cancellationZone: "0x49b91d1d7b9896d28d370b75b92c2c78c1ac984a",
