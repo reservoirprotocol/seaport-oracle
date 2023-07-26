af5aac04ac29dce73d43a512a474c2377b409263
George Roman <george.roman.99@gmail.com>
2023-07-26 10:08:43 +0200

    fix: tweaks

--- packages/react-app/src/utils/contracts.ts
+++ packages/react-app/src/utils/contracts.ts
@@ -9,4 +9,10 @@ export const CONTRACT_ADDRESSES: Record<number, Contracts> = {
   [5]: {
     cancellationZone: "0x49b91d1d7b9896d28d370b75b92c2c78c1ac984a",
   },
+  [137]: {
+    cancellationZone: "0xfb2b693819e866ec87e574903f6e4943723c8ff7",
+  },
+  [80001]: {
+    cancellationZone: "0xfb2b693819e866ec87e574903f6e4943723c8ff7",
+  },
 };
